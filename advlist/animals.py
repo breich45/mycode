2d48325a59e2d2fbba412ec89ad69a05e32b1979
Brent Reich <brent.reich@gmail.com>
2021-05-17 20:46:19 +0000

    End of Day 1

---
+++ advlist/animals.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+animals = ["Fox", "Fly", "Ant", "Bee", "Cod", "Cat", "Dog", "Yak", "Cow", "Hen", "Koi", "Hog", "Jay", "Kit"]
+
+#for i in animals:
+#    print(i)
+
+#mylist = [x for x in animals]
+#print(mylist)
+
+[print(i) for i in animals]
+
